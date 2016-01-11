3dcb4c83779374293a4bc81ad6d2e43b4e58d6b4
Edmundas <edmundas@xtgem.com>
2016-01-11 13:46:44 +0200

    added main grunt file with configured task for watch, web-fonting, precompiling handlebars templates, sass to css compilation

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        webfont: {
+            icons: {
+                src: 'font-icons/src/*.svg',
+                dest: 'font-icons/',
+                options: {
+                    font: 'ficon',
+                    stylesheet: 'scss',
+                    engine: 'node',
+                    relativeFontPath: '../font-icons/',
+                    templateOptions: {
+                        baseClass: 'ficon',
+                        classPrefix: 'ficon-',
+                        mixinPrefix: 'ficon-'
+                    }
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    cacheLocation: 'cache/sass'
+                },
+                files: {
+                    'css/main.css' : 'sass/main.scss'
+                }
+            }
+        },
+        handlebars: {
+            compile: {
+                options: {
+                    namespace: 'hbs',
+                    /* Exclude path then setting template name */
+                    processName: function (filePath) {
+                        return filePath.split('/').pop().split('.')[0];
+                    }
+                },
+                files: {
+                    'js/hbs.js': 'hbs/*.hbs',
+                }
+            }
+        },
+        watch: {
+            webfont: {
+                files: 'font-icons/src/*.svg',
+                tasks: ['webfont', 'sass']
+            },
+            css: {
+                files: 'sass/**/*.scss',
+                tasks: ['sass']
+            },
+            handlebars: {
+                files: 'hbs/*.hbs',
+                tasks: ['handlebars']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-webfont');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
+};
